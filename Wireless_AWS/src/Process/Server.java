97cef27b405bb0a1c5356619ade33f3c4eda476a
Sammy <skupfer@andrew.cmu.edu>
2016-02-28 18:34:47 -0500

    added a website

--- Wireless_AWS/src/Process/Server.java
+++ Wireless_AWS/src/Process/Server.java
@@ -4,6 +4,14 @@ public class Server {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		/*
+		 * TODO: Re-factor and simplify everything:
+		 * -Remove rule checking
+		 * -Simplify config file so it only has the basics: port, IPs, etc.
+		 * -Modify Message, so that it sends a name and GPS coordiantes
+		 * 
+		 */
 
 	}
 
