00070720b93f314d3c8573d007add3a3e68fcd5b
Sammy <skupfer@andrew.cmu.edu>
2016-03-10 18:44:50 -0500

    stupid mistake

--- Wireless_AWS/src/Process/Server.java
+++ Wireless_AWS/src/Process/Server.java
@@ -15,7 +15,7 @@ public class Server {
 		// initialize - get necessary parameters from input
 
 		//config_file_address = "resources/config.txt";
-		config_file_address = "http://http://52.36.135.251/config.txt";
+		config_file_address = "http://52.36.135.251/config.txt";
 		System.out.println("Config_file: " + config_file_address);
 
 		// --------------------------------
