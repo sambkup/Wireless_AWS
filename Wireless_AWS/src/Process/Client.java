f45174e6d962a55bddfba68310ff05479bb628dc
Sammy <skupfer@andrew.cmu.edu>
2016-03-10 18:53:12 -0500

    some modes

--- Wireless_AWS/src/Process/Client.java
+++ Wireless_AWS/src/Process/Client.java
@@ -14,25 +14,26 @@ public class Client {
 		// --------------------------------
 		// initialize - get necessary parameters from input
 
-		config_file_address = "resources/config.txt";
-		// config_file_address = "http://www.andrew.cmu.edu/user/skupfer/config.txt";
-		//System.out.println("Config_file: " + config_file_address);
+		//config_file_address = "resources/config.txt";
+		config_file_address = "http://52.36.135.251/config.txt";
+		System.out.println("Config_file: " + config_file_address);
 
 		// --------------------------------
 		// instantiate the required objects
 
 		config = new Configuration(config_file_address);
-		//config.print();
+		config.print();
 		
 		messagePasser = new MessagePasser(config, false);
 
 		// --------------------------------
 		// Execute the receiver/sender scripts
 
-		Message msg = new Message(54.0678,54.0678,10,10);
+		Message msg = new Message("Sammy",54.0678,54.0678,10,10);
 		messagePasser.send(msg);
+		System.out.println("first message sent");
 
-		msg = new Message(55.0678,55.0678,15,15);
+		msg = new Message("Sammy",55.0678,55.0678,15,15);
 		messagePasser.send(msg);
 		System.exit(0);
 
--- Wireless_AWS/src/communication/Message.java
+++ Wireless_AWS/src/communication/Message.java
@@ -9,23 +9,26 @@ public class Message implements Serializable {
 	double longitude;
 	double vector_x;
 	double vector_y;
+	String sender;
 
-	public Message(double lat, double lon, double vecx, double vecy) {
+	public Message(String sender, double lat, double lon, double vecx, double vecy) {
 		this.lattitude = lat;
 		this.longitude = lon;
 		this.vector_x = vecx;
 		this.vector_y = vecy;
+		this.sender = sender;
 	}
 
 	public void print() {
 		System.out.println("------------------------------");
 		System.out.printf("Coordinates: %f,%f\n",this.lattitude, this.longitude);
 		System.out.printf("Path: <%f,%f>\n", this.vector_x, this.vector_y);
+		System.out.println("Message from " + sender);
 		System.out.println("------------------------------");
 	}
 
 	public Message clone() {
-		Message clone = new Message(this.lattitude, this.longitude, this.vector_x, this.vector_y);
+		Message clone = new Message(this.sender, this.lattitude, this.longitude, this.vector_x, this.vector_y);
 		return clone;
 	}
 }
